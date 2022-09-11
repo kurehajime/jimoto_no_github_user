2e8d5f1deac4c6227a535107711bdec01dd98690
kurehajime <xiidec@gmail.com>
2022-09-11 22:50:54 +0900

    1st commit

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+)
+
+root.render(<div className='container'>
+    <div className='outbox'>
+        hello
+    </div>
+</div>)
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { viteSingleFile } from "vite-plugin-singlefile"
+
+export default defineConfig({
+    base: "./",
+    server: {
+        open: true
+    },
+    build: {
+        outDir: './docs'
+    },
+    assetsInclude: ['**/*.png'],
+    plugins: [react(), viteSingleFile()]
+})
