838bc779dbd96dc98d03285ede713b7e059e3972
kurehajime <xiidec@gmail.com>
2022-09-24 16:05:04 +0900

    シングルファイルにはしない(あんまりでかいとOGPが生成されなくなるから)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteSingleFile } from "vite-plugin-singlefile"
 
 export default defineConfig({
     base: "./",
